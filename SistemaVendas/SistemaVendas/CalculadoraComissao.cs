5eb951bf5f36a2b250dc81502ef1bf5a2950d1ea
cfc <cfc@ufrj.br>
2013-03-08 14:32:45 -0300

    import inicial

---
+++ SistemaVendas/SistemaVendas/CalculadoraComissao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SistemaVendas
+{
+    public class CalculadoraComissao
+    {
+        public static double CalcularComissao(double valor)
+        {
+            double retorno;
+            if (valor <= 10000)
+            {
+                retorno =  0.05 * valor;
+            }
+            else
+            {
+                retorno = 0.06 * valor;
+            }
+
+            return Math.Floor(retorno * 100) / 100; 
+        }
+    }
+}
---
+++ SistemaVendas/TesteVendas/TesteCalculoComissao.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SistemaVendas;
+
+namespace TesteVendas
+{
+    
+    [TestClass]
+    public class TesteCalculoComissao
+    {
+        [TestMethod]
+        public void TesteCalculoComissaoVenda100Retorna5()
+        {
+            double valorVenda = 100;
+            double valorEsperado = 5;
+
+            double retorno = CalculadoraComissao.CalcularComissao(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+        [TestMethod]
+        public void TesteCalculoComissaoVenda200Retorna10()
+        {
+            double valorVenda = 200;
+            double valorEsperado = 10;
+
+            double retorno = CalculadoraComissao.CalcularComissao(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+        [TestMethod]
+        public void TesteCalculoComissaoVenda10000Retorna500()
+        {
+            double valorVenda = 10000;
+            double valorEsperado = 500;
+
+            double retorno = CalculadoraComissao.CalcularComissao(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+        [TestMethod]
+        public void TesteCalculoComissaoVenda100000Retorna6000()
+        {
+            double valorVenda = 100000;
+            double valorEsperado = 6000;
+
+            double retorno = CalculadoraComissao.CalcularComissao(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+        [TestMethod]
+        public void TesteCalculoComissaoVenda200000Retorna12000()
+        {
+            double valorVenda = 200000;
+            double valorEsperado = 12000;
+
+            double retorno = CalculadoraComissao.CalcularComissao(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+
+        [TestMethod]
+        public void TesteCalculoComissaoVendaComProblemaArrendondamento()
+        {
+            double valorVenda = 1.99;
+            double valorEsperado = 0.09;
+
+            double retorno = CalculadoraComissao.CalcularComissao(valorVenda);
+
+            Assert.AreEqual(valorEsperado, retorno);
+        }
+    }
+}
